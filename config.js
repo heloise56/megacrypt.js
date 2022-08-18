3c674447ef1da7aed22af7575831702a4c8c797c
heloise56 <111502060+heloise56@users.noreply.github.com>
2022-08-18 11:21:35 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     // set if domain without port is use as host
-  domain: false,
+  domain: process.env.USE_DOMAIN || false,
     // Set Host
-  host: '127.0.0.1',
+  host: process.env.URL || '127.0.0.1',
     // The port megacrypt.js will be running on
-  port: '3427',
+  port: process.env.PORT || 3000,
     // Server Decryption Key (change this if hosted)
-  serverKey: "PrkqcWVp@/-,R%2P/kEWC-'PgPMHFFM@>EF4x5J-5]}=-:N%rm${dVLf5@3F]/=2",
+  serverKey: process.env.DECRYPTION_KEY || "PrkqcWVp@/-,R%2P/kEWC-'PgPMHFFM@>EF4x5J-5]}=-:N%rm${dVLf5@3F]/=2",
     // if true, the link encryption tool will on the base url
   encryptionTool: true,
     // if true, the link encryption api will be enabled (nedded for the encryption tool)
